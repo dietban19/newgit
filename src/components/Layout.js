a34394d24aeccd995cb5fee2ef497815048c1a74
dietban19 <dieter.banaag@ucalgary.ca>
2023-04-16 16:01:37 -0600

    fix: changed up stylings

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,7 +13,7 @@ function Layout() {
   const audioRef = useRef(null);
 
 
-    // selectedFile is the image that was choosen by the user
+  // selectedFile is the image that was choosen by the user
   const [selectedFile, setSelectedFile] = useState('');
   const [selectedFileName, setSelectedFileName] = useState('');
 
@@ -39,10 +39,15 @@ function Layout() {
     }
     return deviceId;
   };
-  
+
   const deviceId = getDeviceId();
 
-  
+  /**
+   * Asynchronously fetches the obituaries associated with the current device ID.
+   * @async
+   * @function
+   * @returns {Promise<void>}
+   */
   useEffect(() => {
     console.log("LOADING")
     const fetchObituaries = async () => {
@@ -60,7 +65,6 @@ function Layout() {
           const obituaries = await response.json();
           setFetchedObituaries(obituaries);
         } else {
-          // Handle non-200 status codes as needed
           console.error(`Error fetching obituaries: ${response.status}`);
         }
       } catch (error) {
@@ -69,13 +73,13 @@ function Layout() {
     };
 
     fetchObituaries();
-  }, []); // Add any dependencies as needed
-  
+  }, []); 
+
   console.log(fetchedObituaries)
   useEffect(() => {
     if (imageUrl && gptDescription && audioUrl) {
       const newObituary = {
-        deviceID : deviceId,
+        deviceID: deviceId,
         id: uuidv4(),
         name: name,
         image: imageUrl, // Use the transformed image URL
@@ -103,7 +107,7 @@ function Layout() {
     navigate("/obituaries");
   }, [obituaries, navigate]);
 
-  
+
 
   const openPopup = () => {
     setIsPopupOpen(true);
@@ -121,7 +125,7 @@ function Layout() {
 
     setIsPopupOpen(false);
   };
-  
+
   useEffect(() => {
     const body = document.querySelector("body");
     if (isPopupOpen) {
@@ -134,39 +138,39 @@ function Layout() {
   //   const baseUrl = "https://res.cloudinary.com/dx0n3s9h4/image/upload";
   //   return `${baseUrl}/${effect}/${publicId}`;
   // };
-  
+
   const handleNameChange = (event) => {
     setName(event.target.value);
   };
   const handleBirthDateChange = (event) => {
     setBirthDate(event.target.value);
   };
-  
+
   const handleDeathDateChange = (event) => {
     setDeathDate(event.target.value);
   };
 
-const handleFileChange = (event) => {
-  if (event.target.files.length > 0) {
-    setSelectedFile(event.target.files[0]);
-    setSelectedFileName(event.target.files[0].name);
-  } else {
-    setSelectedFile(null);
-    setSelectedFileName(null);
-  }
-  console.log("SELECTED FILE IS" ,selectedFile, "OTHER IS \n",selectedFileName);
-};
+  const handleFileChange = (event) => {
+    if (event.target.files.length > 0) {
+      setSelectedFile(event.target.files[0]);
+      setSelectedFileName(event.target.files[0].name);
+    } else {
+      setSelectedFile(null);
+      setSelectedFileName(null);
+    }
+    console.log("SELECTED FILE IS", selectedFile, "OTHER IS \n", selectedFileName);
+  };
 
-// const handleInputChange = (setStateFunction, event) => {
-//   setStateFunction(event.target.value);
-// }; 
+  // const handleInputChange = (setStateFunction, event) => {
+  //   setStateFunction(event.target.value);
+  // }; 
 
-// const playAudio = () => {
-//   if (audioRef.current) {
-//     audioRef.current.play();
-//   }a
-// };
-const handleWriteObituary = async () => {
+  // const playAudio = () => {
+  //   if (audioRef.current) {
+  //     audioRef.current.play();
+  //   }a
+  // };
+  const handleWriteObituary = async () => {
     setLoading(true)
     if (birthDate && deathDate) {
       const formData = new FormData();
@@ -179,51 +183,51 @@ const handleWriteObituary = async () => {
 
 
 
-    //the code creates a set of key/value pairs that can be sent as part of an HTTP request. 
-    console.log("LOADING",loading)
-    try {
-      // sends an HTTP POST request to the URL 
-      //passing in the formData object as the request body.
-      const response = await axios.post(
-        "https://tzduudnsqngo3jwtcb47kzsuva0xuuml.lambda-url.ca-central-1.on.aws/",
-        formData
-        //
-        , //By passing in the formData object as the request body, the code is 
-              //sending a set of key/value pairs representing form data to the specified URL.
-        {
-          //object containing headers for the request.
-          headers: {
-            "content-type": "multipart/form-data",
-          },
-        }
-      );
-      
-      // console.log("THE RESPONSE: ",response);
-      setImageUrl(response.data.image_url);
-      setGptDescription(response.data.gpt_response)
-      setAudioUrl(response.data.audio_file)
+      //the code creates a set of key/value pairs that can be sent as part of an HTTP request. 
+      console.log("LOADING", loading)
+      try {
+        // sends an HTTP POST request to the URL 
+        //passing in the formData object as the request body.
+        const response = await axios.post(
+          "https://tzduudnsqngo3jwtcb47kzsuva0xuuml.lambda-url.ca-central-1.on.aws/",
+          formData
+          //
+          , //By passing in the formData object as the request body, the code is 
+          //sending a set of key/value pairs representing form data to the specified URL.
+          {
+            //object containing headers for the request.
+            headers: {
+              "content-type": "multipart/form-data",
+            },
+          }
+        );
 
-      if (response.status === 200) {  
-        // console.log("ayy")
-      } else {
+        // console.log("THE RESPONSE: ",response);
+        setImageUrl(response.data.image_url);
+        setGptDescription(response.data.gpt_response)
+        setAudioUrl(response.data.audio_file)
+
+        if (response.status === 200) {
+          // console.log("ayy")
+        } else {
+          alert("Error occurred while creating obituary.");
+        }
+
+      } catch (error) {
+        console.error("Error calling the create-obituary Lambda function:", error);
         alert("Error occurred while creating obituary.");
       }
-      
-    } catch (error) {
-      console.error("Error calling the create-obituary Lambda function:", error);
-      alert("Error occurred while creating obituary.");
+      setLoading(false)
+    } else {
+      alert("Please enter both birth and death date/time");
     }
-    setLoading(false)
-  } else {
-    alert("Please enter both birth and death date/time");
-  }
-  console.log(loading)
-};
+    console.log(loading)
+  };
 
 
 
-  
- 
+
+
   return (
     <div id="container">
       <header>
@@ -236,121 +240,130 @@ const handleWriteObituary = async () => {
         </aside>
 
         <div id="top-header">
-            <Link to="/notes">The Last Show</Link>
+          <Link to="/notes">The Last Show</Link>
         </div>
-        
+
         <aside>
-          <button id = "add-button" onClick={openPopup}>+ Add Obituary</button>
+          <button id="add-button" onClick={openPopup}>+ Add Obituary</button>
         </aside>
       </header>
-      <div>
-    </div>
-      <div id="main-container" ref={mainContainerRef}>
-      
-      <div>
-   
-      {isPopupOpen && (
-        <>
-        
-        <div className = "popup">
-        {/* <div className="loading">Loading...</div> */}
-        {/* {loading && <div className="loading">Loading...</div>} */}
-        
-          <div id = "popup-top"> 
-              <button id = "popup-button" onClick={closePopup}><span className = "icon">&lt;</span> Back</button>
-          </div>
-          <div className = "popup-container">
-              <div className = "popup-header">
-                <h1>Create a New Obituary</h1>
-                 <br></br>
-              </div>
-              <div className = "popup-info">
-                   <div className = "image-input">
-                      <input
-                        type="file"
-                        id="file"
-                        accept="image/*"
-                        onChange={(event)=>{handleFileChange(event)}}
-                        disabled={loading}
-                      ></input>
-                      <label htmlFor="file" id="choose-image">
-                        <div id="select-image">
-                          <i className="select-image-icon">
-                            <RiAddCircleLine />
-                          </i>
-                          Select an Image for the Deceased
-                        </div>
-                      </label>
 
-                      <span>{selectedFile && `Selected file: ${selectedFileName}`}</span>
-                    </div>
-                <div className = "popup-contents-main">
-                <input
-                    id="input-name"
-                    type="text"
-                    value={name}
-                    onChange={(event) => handleNameChange(event)}
-                    disabled={loading}
-                  />
-                  <div id = "date-container">
-                  <h3>Born:{" "}
-                  <input type="datetime-local"
-                          value={birthDate}
-                          onChange={(event) => handleBirthDateChange(event)}
-                          disabled={loading}
-                          ></input></h3>
-                  <h3>
-                    Died:{" "}
-                    <input
-                      type="datetime-local"
-                      value={deathDate}
-                      onChange={(event) => handleDeathDateChange(event)}
-                      disabled={loading}
-                    ></input>
-                  </h3>
+      <div id="main-container" ref={mainContainerRef}>
+        <body>
+          <div>
+            {isPopupOpen && (
+              <>
+
+                <div className="popup">
+                  {/* <div className="loading">Loading...</div> */}
+                  {/* {loading && <div className="loading">Loading...</div>} */}
+
+                  <div id="popup-top">
+                    <button id="popup-button" onClick={closePopup}><span className="icon">&lt;</span> Back</button>
                   </div>
-                  
+                  <div className="popup-container">
+                    <div className="popup-header">
+                      <h1>Create a New Obituary</h1>
+                      <br></br>
+                    </div>
+                    <div className="popup-info">
+                      <div className="image-input">
+                        <input
+                          type="file"
+                          id="file"
+                          accept="image/*"
+                          onChange={(event) => { handleFileChange(event) }}
+                          disabled={loading}
+                        ></input>
+                        <label htmlFor="file" id="choose-image">
+                          <div id="select-image">
+                            <i className="select-image-icon">
+                              <RiAddCircleLine />
+                            </i>
+                            Select an Image for the Deceased
+                          </div>
+                        </label>
+
+                        <span>{selectedFile && `Selected file: ${selectedFileName}`}</span>
+                      </div>
+                      <div className="popup-contents-main">
+                        <input
+                          id="input-name"
+                          type="text"
+                          value={name}
+                          onChange={(event) => handleNameChange(event)}
+                          disabled={loading}
+                        />
+                        <div id="date-container">
+                          <h3>Born:{" "}
+                            <input type="datetime-local"
+                              value={birthDate}
+                              onChange={(event) => handleBirthDateChange(event)}
+                              disabled={loading}
+                            ></input></h3>
+                          <h3>
+                            Died:{" "}
+                            <input
+                              type="datetime-local"
+                              value={deathDate}
+                              onChange={(event) => handleDeathDateChange(event)}
+                              disabled={loading}
+                            ></input>
+                          </h3>
+                        </div>
+
+                      </div>
+                    </div>
+                    <div className="popup-bottom">
+                      <div id="popup-button">
+                        <button
+                          onClick={handleWriteObituary}
+                          disabled={!birthDate || !deathDate || !selectedFile || !name || loading}
+                          className={!birthDate || !deathDate || !name || !selectedFile || loading ? "button-disabled" : "button-enabled"}>
+                          {loading ? "Loading..." : "Write Obituary"}
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className = "popup-bottom">
-                <div id = "popup-button">
-                  <button
-                      onClick={handleWriteObituary}
-                      disabled={!birthDate || !deathDate || !selectedFile||!name || loading}
-                      className={!birthDate || !deathDate ||!name || !selectedFile || loading ? "button-disabled" : "button-enabled"}>
-                     {loading ? "Loading..." : "Write Obituary"}
-                  </button>
-                </div>
-              </div>
-          </div>
-        </div>
-        <div className="blur-background"></div>;
-        
-        
-        </>
-      )}
-           <div className = "main-container">
-           <Obituary obituaries={obituaries}/>
-        </div>
-    </div>
-    <Outlet />
-        {/* <aside id="sidebar" className={collapse ? "hidden" : null}>
-          <header>
-            <div id="notes-list-heading">
-              <h2>Notes</h2>
-              <button id="new-note-button" onClick={addNote}>
-                +
-              </button>
+                <div className="blur-background"></div>;
+
+
+              </>
+            )}
+            <div className="main-container">
+              <Obituary obituaries={obituaries} />
             </div>
-          </header>
-          <div id="notes-holder">
-            <NoteList notes={notes} />
           </div>
-        </aside> */}
+          <Outlet />
+        </body>
 
       </div>
+      <footer>
+        <table>
+          <th><h4>Contact Us</h4></th>
+          <tr>
+            <td>123 Main Street</td>
+            <td>Anytown, USA 12345</td>
+          </tr>
+          <tr>
+            <td>Column 1</td>
+            <td>Column 2</td>
+          </tr>
+
+        </table>
+
+        <div className="row2">
+          <div class="col-2">
+            <p>&copy; 2023 Dieter Banaag. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
+
     </div>
+
   );
+
 }
 
 export default Layout;
